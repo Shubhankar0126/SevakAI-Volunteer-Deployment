fa51a5378dc9d5a35404e4b3e0d424cdd4640580
Shubhankar Pandey <shubhankarpandey322@gmail.com>
2026-07-08 22:41:19 +0530

    chore: use standard tanstack configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackRouterPlugin } from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  vite: {
-    server: {
-      allowedHosts: true,
-    },
-  },
-
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-});
+  plugins: [
+    tanstackRouterPlugin(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+})
