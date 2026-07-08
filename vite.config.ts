f01293299e7bcd6b7152a714a7b9d1d35455c2b2
Shubhankar Pandey <shubhankarpandey322@gmail.com>
2026-07-08 19:52:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/start/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart()
   ],
 });
